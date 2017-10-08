cb84fcbbcc9f3d9d377eaf8d2f90624cbbebccaa
Justin <developer.variety@gmail.com>
2017-10-08 13:55:53 -0400

    Disposing SHA256

--- EZLib/HardwareId.cs
+++ EZLib/HardwareId.cs
@@ -27,12 +27,13 @@ namespace EZLib
             hardwareId = hddModel + hddManufacturer + hddSerialNumber + motherboardModel + motherboardManufacturer +
                          motherboardNumber;
 
-            var sha256Managed = new SHA256Managed();
+            using (var sha256Managed = new SHA256Managed())
+            {
+                var key = sha256Managed.ComputeHash(Encoding.ASCII.GetBytes("EZLIB_HWID"));
+                var iv = new byte[16] { 0x8, 0x2, 0x7, 0x4, 0x55, 0x1, 0x6, 0x9, 0x4, 0x92, 0x90, 0x22, 0x65, 0x2, 0x9, 0x1 };
 
-            var key = sha256Managed.ComputeHash(Encoding.ASCII.GetBytes("EZLIB_HWID"));
-            var iv = new byte[16] {0x8, 0x2, 0x7, 0x4, 0x55, 0x1, 0x6, 0x9, 0x4, 0x92, 0x90, 0x22, 0x65, 0x2, 0x9, 0x1};
-
-            return Cryptography.Encrypt(hardwareId, key, iv);
+                return Cryptography.Encrypt(hardwareId, key, iv);
+            }
         }
 
         private static string getComponentId(string hwclass, string syntax)
