b62b53d06a51482cf981a91fa50f65272ea4bb80
Justin <developer.variety@gmail.com>
2017-10-08 11:33:39 -0400

    https://github.com/developer-variety/EZLib-PHP

--- Web_Server/api/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-
-echo("<h1>EZLib API</h1>");
--- Web_Server/sign_in.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require $_SERVER['DOCUMENT_ROOT'] . "/Web_Server/includes/header.php";
-
-?>
-
-
-
-<?php require $_SERVER['DOCUMENT_ROOT'] . "/Web_Server/includes/footer.php"; ?>
